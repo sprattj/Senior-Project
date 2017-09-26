2f5ef6d0645ded856beedff007276e17d9b1d0ad
Paul-Kyle-Turner <turnerp5@students.rowan.edu>
2017-09-26 19:01:53 -0400

    Fix some

--- DropZoneHQ/DropZoneHQ/settings.py
+++ DropZoneHQ/DropZoneHQ/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Gear.apps.GearConfig',
 ]
 
 MIDDLEWARE = [
@@ -78,6 +79,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    #MySQL
+    #'default':{
+    #    ''
+    #}
 }
 
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
