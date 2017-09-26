fdd3a06458ca66778bd773a4ddd8a036974d0421
Paul-Kyle-Turner <turnerp5@students.rowan.edu>
2017-09-26 19:03:53 -0400

    add models

--- DropZoneHQ/Gear/admin.py
+++ DropZoneHQ/Gear/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Employee
+from .models import Instructor
+from .models import Rigger
+from .models import Packer
+from .models import Sign_Out
