938b289d7dff968c64d40ef1bcc31cd27218cfdd
Paul-Kyle-Turner <turnerp5@students.rowan.edu>
2017-10-14 17:28:00 -0400

    i can get it to work one of these times

---
+++ backend/datastore/tests.py
@@ -0,0 +1,43 @@
+# from DropZoneHQ.models import Rigs, Canopies, AllCanopies, AllItems, Items
+# from dropZoneHQ.views import RigViewSet
+from django.utils.six import BytesIO
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
+from backend.datastore.serializers import *
+
+# print(Canopies.objects.all().values().__str__())
+# print(RigViewSet.get_queryset(RigViewSet.serializer_class).values().__str__())
+# print(AllCanopies.objects.all().values().__str__())
+# print(AllItems.objects.all().values().__str__())
+
+
+signouts = EmployeesVsSignouts.objects.all()
+serializer = EmployeeVsSignoutSerializer(signouts, many=True)
+print(serializer.data)
+
+'''Get an object'''
+item = Items.objects.all().get(item_id=2)
+'''Serialize an object'''
+serializer = ItemSerializer(item)
+'''Render serialized data into JSON'''
+content = JSONRenderer().render(serializer.data)
+# test json
+print(content)
+'''Deserialization:
+    Convert JSON content into a stream'''
+stream = BytesIO(content)
+'''Parse the stream into deserialized data'''
+data = JSONParser().parse(stream)
+# test data
+print(data)
+
+serializer = ItemSerializer(data=data)
+print(serializer.is_valid())
+print(serializer.errors.__str__())  # Empty: No errors
+print(serializer.validated_data)
+# serializer.save()
+
+serializer = ItemSerializer(Items.objects.all(), many=True)
+print(serializer.data)
+
