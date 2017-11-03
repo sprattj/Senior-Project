14790d53394cb468e2cfe5a3c0b5e179065ee739
Paul-Kyle-Turner <turner.paul.k@gmail.com>
2017-11-03 19:38:12 -0400

    mac commit

--- backend/dropZoneHQ/auth.py
+++ backend/dropZoneHQ/auth.py
@@ -1,7 +1,8 @@
 #
-from Crypto.Hash import SHA256, Random
-from backend.datastore.models import Dropzones
-from django.http import HttpResponse, HttpRequest
+from Crypto.Hash import SHA256
+from Crypto import Random
+from backend.datastore.models import Dropzones, Employees
+from django.http import JsonResponse, HttpRequest
 from django.shortcuts import get_object_or_404
 
 class Auth:
@@ -9,11 +10,12 @@ class Auth:
 
     # authenticate a dropzone based on the name and password
     #return None if there is no dropzone there
-    def authenticateDropzone(self, request, name=None, password=None):
-        check = self.authenticatePasswordDropzone(self.authenticateNameDropzone(name), password)
-        return (check if check is not None else HttpResponse(status=401))
+    def authenticateDropzone(self, request):
+        check = self.authenticatePasswordDropzone(self.authenticateNameDropzone(request.POST['username']), request.POST['password'])
+        return (check if check is not None else JsonResponse(status=401))
     #
-    def authenticateDropzone(self, request, token=None):
+    def authenticateDropzone(self, request):
+        token = request.session['sessionID']
         return False
 
     #return a user object if the username is found
@@ -22,11 +24,8 @@ class Auth:
         if name is None :
             return None
         else:
-            dropzone_query = Dropzones.objects.all()
-            for dropzonei in dropzone_query :
-                if name == dropzonei.name :
-                    return dropzonei
-            return None
+            dropzone_query = Dropzones.objects.get(name)
+            return dropzone_query
 
     #return a dropzone if the Sha is correct
     #else return None
@@ -39,8 +38,40 @@ class Auth:
             else :
                 return None
 
-    def authenticateUserPin(self, request, user=None):
-        return False
+    def authenticateUserPin(self, request):
+        pin = SHA256.new(data = bytes(request.POST['pin']))
+        emp = Employees.objects.all()
+        for e in emp :
+            if e.pin == pin :
+                return JsonResponse(data=e,status=200)
+        return JsonResponse(status=400)
+
+    def createDropzone(self, request):
+        name = request.POST['name']
+        password = request.POST['password']
+        location = request.POST['location']
+        if self.dropzoneNameInUse(name) is not None:
+            if self.dropzoneLocationInUse() is not None:
+                dropzone = Dropzones(name=name, password=password, location=location)
+                dropzone.save()
+                return JsonResponse(status=201)
+        return JsonResponse(status=400)
+
+    # Checks if a location is in use for a dropzone.
+    def dropzoneLocationInUse(self, location=None):
+        use = Dropzones.objects.get(location)
+        if use is not None:
+            return use
+        else:
+            return None
+
+    #Checks if a name is in use for a dropzone.
+    def dropzoneNameInUse(self,name=None):
+        use = Dropzones.objects.get(name)
+        if use is not None :
+            return use
+        else :
+            return None
 
     #Employee random generation of 4 digit pin exclusive to dropzone
     def randomUserPin(self, userPK = None):
