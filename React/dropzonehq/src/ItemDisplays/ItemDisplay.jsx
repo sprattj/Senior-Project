20e98592c798058ccc0c16283f20b4dba632adae
admin <bkrickjr@gmail.com>
2017-11-05 16:10:09 -0500

    Beside actual communication with endpoints, so rentFunction and returnFunction, Rentals is fully functional with a filter and data passing thru the screen from the table to the display WOW!

--- React/dropzonehq/src/ItemDisplays/ItemDisplay.jsx
+++ React/dropzonehq/src/ItemDisplays/ItemDisplay.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import { Row, Col, Card, CardHeader, CardBlock } from 'reactstrap';
 
-export default class ItemDisplay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {            
-            headerText: this.props.headerText,
-            statsToDisplay: this.props.statsToDisplay
-        }
-    }
+export default class ItemDisplay extends React.Component {    
 
     render() {
         return (
@@ -16,9 +9,9 @@ export default class ItemDisplay extends React.Component {
                 <Row>
                     <Col>
                         <Card >
-                            <CardHeader>{this.state.headerText}</CardHeader>
+                            <CardHeader>{this.props.headerText}</CardHeader>
                             <CardBlock>
-                                {this.state.statsToDisplay}
+                                {this.props.statsToDisplay}
                             </CardBlock>
                         </Card>
                     </Col>
--- React/dropzonehq/src/ItemDisplays/RentalItemDisplay.jsx
+++ React/dropzonehq/src/ItemDisplays/RentalItemDisplay.jsx
@@ -4,41 +4,13 @@ import RentButton from '../Buttons/RentButton.jsx';
 import { Row, Col } from 'reactstrap';
 import { rootURL } from '../restInfo.js';
 
-export default class RiggingItemDisplay extends React.Component {
+export default class RentalItemDisplay extends React.Component {
     constructor(props) {
         super(props);
         //since the URL section is not directly related to rendering,
         //it shouldn't be part of state. Save it in a class variable.
         this.URLsection = "/rentalitemdisplay";
 
-        this.setRButton = this.setRButton.bind(this);
-
-        this.state = {
-            RButton: null
-        }
-
-    }
-
-    setRButton() {
-        console.log("RentalItemDisplay: setRButton: this.props.isRented");
-        console.log(this.props.isRented);
-        if (this.props.isRented) {
-            this.setState({
-                RButton: "Rented Out"
-                //RButton: <RentButton buttonText={"Return"} />
-            })
-        } else {
-            this.setState({
-                RButton: "Available"
-                //RButton: <RentButton buttonText={"Rent"} />
-            })
-        }
-        console.log(this.state.RButton);
-    }
-
-    componentDidMount() {
-        console.log("RentalItemDisplay: componentDidMount")
-        this.setRButton();
     }
 
     render() {
@@ -53,17 +25,20 @@ export default class RiggingItemDisplay extends React.Component {
                                     <Row>
                                         <Col>
                                             <div>
-                                                <p>The Item rowID Is: {this.props.rowID}</p>
+                                                <p>Item Number: {this.props.number}</p>
                                                 <p>The Person Currently Renting This Item Is: {this.props.renterName}</p>
                                             </div>
                                         </Col>
                                         <Col>
+                                            <div>
                                             <p>Item Description: {this.props.desc}</p>
+                                            <p>Item Type: {this.props.type}</p>
+                                            </div>
                                         </Col>
                                     </Row>
                                     <Row>
                                         <Col>
-                                            {this.state.RButton}
+                                            {this.props.button}
                                         </Col>
                                     </Row>
                                 </div>
