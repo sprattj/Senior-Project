c5b4847896d66bcee9a761edfdc248c6df648d0a
JatinB77 <bhaktaj7@students.rowan.edu>
2017-11-06 12:09:44 -0500

    added edit functionality and add btn to inventory

--- React/dropzonehq/src/ItemDisplays/EditInventoryItemDisplay.jsx
+++ React/dropzonehq/src/ItemDisplays/EditInventoryItemDisplay.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ItemDisplay from './ItemDisplay.jsx';
 import RentButton from '../Buttons/RentButton.jsx';
-import { Row, Col } from 'reactstrap';
+// import EditableInput from 'react-editable-input';
+// import PropTypes from 'prop-types';
+import { Form, FormGroup, Input, Row, Col, InputGroup, InputGroupAddon } from 'reactstrap';
 import { rootURL } from '../restInfo.js';
+// import InlineEdit from 'react-edit-inline';
 
+
+// var EditableInput = require('react-editable-input').default; 
 export default class EditInventoryItemDisplay extends React.Component 
 {
     constructor(props) 
@@ -12,9 +17,32 @@ export default class EditInventoryItemDisplay extends React.Component
         //since the URL section is not directly related to rendering,
         //it shouldn't be part of state. Save it in a class variable.
         this.URLsection = "/editInventoryItemDisplay";
+
+        this.inventoryItemNumChanged = this.inventoryItemNumChanged.bind(this);
+        this.state = {
+            itemNum:  this.props.number //'' // initially is number that's passed
+        }
+    }
+
+    inventoryItemNumChanged(e) {
+        this.setState({ itemNum: this.props.number // saved into state var, so it rerenders
+            // itemNum: e.target.value,
+        });
+        
+        // var editInvItem = document.getElementById('editInventoryItem');
+        // editInvItem.value = this.state.itemNum;
+      }
+
+    saveToState()
+    {
+        this.setState({ itemNum: this.props.number // saved into state var, so it rerenders
+            // itemNum: e.target.value,
+        });
+       // return this.state.itemNum;
     }
 
     render() {
+
         return (
             <div>
                 <Row>
@@ -25,15 +53,29 @@ export default class EditInventoryItemDisplay extends React.Component
                                     <Row>
                                         <Col>
                                             <div>
-                                                <p> The Item #: {this.props.number} </p>
-                                                <p>The Person Currently Renting This Item Is: {this.props.renterName}</p>
-                                                <input type='textbox' value= {this.props.number} disabled={true} />
+                                                <InputGroup>
+                                                    <InputGroupAddon >Item #: </InputGroupAddon>
+                                                    <Input id="editInventoryItem" type='text' value={this.props.number} onChange={this.inventoryItemNumChanged}  />
+                                                </InputGroup>
+                                                <br />
+                                                <InputGroup>
+                                                    <InputGroupAddon >Rented By: </InputGroupAddon>
+                                                    <Input id="itemRenterName" type='text' value={this.props.renterName} onChange={this.inventoryItemNumChanged}  />
+                                                </InputGroup>
+                                                
                                             </div>
                                         </Col>
                                         <Col>
                                             <div>
-                                            <p>Item Description: {this.props.desc}</p>
-                                            <p>Item Type: {this.props.type}</p>
+                                                <InputGroup>
+                                                    <InputGroupAddon >Item Description: </InputGroupAddon>
+                                                    <Input id="itemDesc" type='text' value={this.props.desc} onChange={this.inventoryItemNumChanged}  />
+                                                </InputGroup>
+                                                <br />
+                                                <InputGroup>
+                                                    <InputGroupAddon >Item Type: </InputGroupAddon>
+                                                    <Input id="itemType" type='text' value={this.props.type} onChange={this.inventoryItemNumChanged}  />
+                                                </InputGroup>
                                             </div>
                                         </Col>
                                     </Row>
