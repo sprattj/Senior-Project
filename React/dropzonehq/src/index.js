1e8af2a184efd6617976ef4126ec2f4ba2372653
bkrickjr <bkrickjr@gmail.com>
2017-11-20 13:22:01 -0500

    need that semi colon

--- React/dropzonehq/src/index.js
+++ React/dropzonehq/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import MainView from './Screens/MainView.jsx';
 import './index.css';
