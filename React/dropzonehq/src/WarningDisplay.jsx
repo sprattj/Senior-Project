9f46786842bc8c5978098f9af49470cab6d6c358
Paul Bayruns <bayrunsp9@students.rowan.edu>
2017-11-26 17:56:25 -0500

    improved loft display views

--- React/dropzonehq/src/WarningDisplay.jsx
+++ React/dropzonehq/src/WarningDisplay.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
-import TextArea from './TextInputs/TextArea.jsx';
+import { CLAIM_SEVERITY_CHOICES } from './restInfo.js';
+import { Alert } from 'reactstrap';
 
 export default class WarningDisplay extends React.Component {
 
     render(){
 
-        return(
+        var alertColor = "secondary";
+        var alertMsg = "Issue";
+        if (this.props.severity) {
+            switch (this.props.severity.toLowerCase()) {
+                case CLAIM_SEVERITY_CHOICES.CRITICAL.toLowerCase():
+                    alertColor = "danger";
+                    alertMsg = "Critical Issue";
+                    break;
+                case CLAIM_SEVERITY_CHOICES.NON_CRITICAL.toLowerCase():
+                    alertColor = "warning";
+                    alertMsg = "Moderate Severity Issue";
+                    break;
+                case CLAIM_SEVERITY_CHOICES.COSMETIC.toLowerCase():
+                    alertColor = "dark";
+                    alertMsg = "Cosmetic Issue";
+                    break;
+            }
+        }
+
+        return (
             <div>
-            <ul>
-                <li>Rig:</li>
-                <li>Reported by:</li>
-                <li><TextArea id="warningDisplayTextArea" labelText="Problem:"/></li>
-            </ul>
+                <h3>Pending Claim Issue</h3>
+                <Alert color={alertColor}>
+                    <h4>Rig {this.props.rig_id}: {alertMsg}</h4>
+                    <br />
+                    <h5>Description:</h5>
+                    <p>{this.props.description}</p>
+                    <br />
+                    <h5>Submit Date: {this.props.submit_date}</h5>
+                    <br />
+                    <h5>Due Date: {this.props.due_date}</h5>
+                </Alert>
             </div>
         );
     }
