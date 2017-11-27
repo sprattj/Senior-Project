51ac02d7f30ee94281bb6edf3499f4be8bb344c4
andres-blotta <32212966+andres-blotta@users.noreply.github.com>
2017-11-26 22:09:09 -0500

    Fixed a couple errors in Employee Admin Screen

--- React/dropzonehq/src/Tables/EmployeeTable.jsx
+++ React/dropzonehq/src/Tables/EmployeeTable.jsx
@@ -160,7 +160,7 @@ export default class EmployeeTable extends React.Component {
             id={employee_id}
             authorize={self.editEmployee} />
           <EmployeeStatusButton
-            employee_id={employee_id}
+            id={employee_id}
             toggleEmployeeStatus={self.toggleEmployeeStatus}
             firstName={firstName}
             lastName={lastName}
